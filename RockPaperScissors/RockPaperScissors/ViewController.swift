70434e177a89ecda0f79b136293fe6fab8a838f9
youngwoo_ahn <codeway119@gmail.com>
2024-06-03 16:23:34 +0900

    project init

---
+++ RockPaperScissors/RockPaperScissors/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+#Preview { ViewController() }
+
+class ViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .systemGray
+	}
+}
+
