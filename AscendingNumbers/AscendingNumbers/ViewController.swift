01300866b456ee0e71990bcca456fa3b168d1c5f
the-hye <ganzi410@gmail.com>
2024-06-03 15:29:13 +0900

    Initialize Project && Add Image Assets

---
+++ AscendingNumbers/AscendingNumbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AscendingNumbers
+//
+//  Created by JIHYE SEOK on 6/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
