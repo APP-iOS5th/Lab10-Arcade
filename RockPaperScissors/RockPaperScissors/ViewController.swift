36fa27b73e6a6728b899b85ba5aa0cd97b786331
youngwoo_ahn <codeway119@gmail.com>
2024-06-04 09:44:29 +0900

    background image setting

--- RockPaperScissors/RockPaperScissors/ViewController.swift
+++ RockPaperScissors/RockPaperScissors/ViewController.swift
@@ -6,9 +6,32 @@ class ViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = .systemGray
+		view.backgroundColor = .white
+		self.title = "Main"
+		
+		let appearence = UINavigationBarAppearance()
+		appearence.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+		appearence.backgroundColor = .systemBrown
+		self.navigationController?.navigationBar.scrollEdgeAppearance = appearence
+		
+		let button = UIButton(type: .system)
+		button.setTitle("Go to Game", for: .normal)
+		button.addTarget(self, action: #selector(goNext), for: .touchUpInside)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(button)
+		
+		NSLayoutConstraint.activate([
+			button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+		])
+
 	}
 }
 
-
+extension ViewController {
+	@objc func goNext() {
+		let nextVC = RockPaperScissorsViewController()
+		navigationController?.pushViewController(nextVC, animated: true)
+	}
+}
 
--- RockPaperScissors/RockPaperScissors/Views/RockPaperScissorsViewController.swift
+++ RockPaperScissors/RockPaperScissors/Views/RockPaperScissorsViewController.swift
@@ -1,16 +1,45 @@
-//
-//  RockPaperScissorsViewController.swift
-//  RockPaperScissors
-//
-//  Created by user on 6/3/24.
-//
-
 import UIKit
 
+#Preview { RockPaperScissorsViewController() }
+
 class RockPaperScissorsViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .systemBackground
+		
+		setupNavigationBar()
+		
+		let backgroundImage = UIImageView()
+		backgroundImage.image = UIImage(
+			named: "background_multiplier_2.5")
+		backgroundImage.contentMode = .scaleAspectFit
+		backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(backgroundImage)
+		view.sendSubviewToBack(backgroundImage)
+		
+		NSLayoutConstraint.activate([
+			backgroundImage.widthAnchor.constraint(
+				equalTo: view.widthAnchor),
+			backgroundImage.heightAnchor.constraint(
+				equalTo: backgroundImage.widthAnchor,
+				multiplier: 2.5),
+			backgroundImage.bottomAnchor.constraint(
+				equalTo: view.bottomAnchor),
+		])
+	}
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
+// MARK: - setup
+extension RockPaperScissorsViewController {
+	func setupNavigationBar() {
+		self.title = "RockPaperScissors"
+		let appearence = UINavigationBarAppearance()
+		appearence.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+		appearence.backgroundColor = .systemBrown
+		self.navigationController?.navigationBar.scrollEdgeAppearance = appearence
+	}
+	
+	
+	
 }
