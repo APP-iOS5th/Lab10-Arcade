a91addfd6f270b20ffe4b94a94941e58da0f0bc9
youngwoo_ahn <codeway119@gmail.com>
2024-06-04 09:52:38 +0900

    background image setting

--- RockPaperScissors/RockPaperScissors/ViewController.swift
+++ RockPaperScissors/RockPaperScissors/ViewController.swift
@@ -3,28 +3,37 @@ import UIKit
 #Preview { ViewController() }
 
 class ViewController: UIViewController {
-	
+	let button = UIButton(type: .system)
+}
+
+extension ViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		view.backgroundColor = .white
+		setupNavigationBar()
+		setupButton()
+	}
+}
+
+extension ViewController {
+	func setupNavigationBar() {
 		self.title = "Main"
-		
 		let appearence = UINavigationBarAppearance()
 		appearence.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
 		appearence.backgroundColor = .systemBrown
 		self.navigationController?.navigationBar.scrollEdgeAppearance = appearence
-		
-		let button = UIButton(type: .system)
+	}
+	
+	func setupButton() {
 		button.setTitle("Go to Game", for: .normal)
 		button.addTarget(self, action: #selector(goNext), for: .touchUpInside)
-		button.translatesAutoresizingMaskIntoConstraints = false
 		view.addSubview(button)
 		
+		button.translatesAutoresizingMaskIntoConstraints = false
 		NSLayoutConstraint.activate([
 			button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
 			button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
 		])
-
 	}
 }
 
