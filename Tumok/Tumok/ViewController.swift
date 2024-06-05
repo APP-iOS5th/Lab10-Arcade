b8387a2c289014d2db07582bd570f904840df9e2
Jongchan Yun <jcyun1996@gmail.com>
2024-06-05 09:45:35 +0900

    Initial Tumok

---
+++ Tumok/Tumok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tumok
+//
+//  Created by John Yun on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
