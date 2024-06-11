15d39b0b1eb08032a309b80c6fcf57fd93b12a99
Jongchan Yun <jcyun1996@gmail.com>
2024-06-11 22:09:07 +0900

    Seperate Folders

--- LetsPlay/LetsPlay/Models/GameModel.swift
+++ LetsPlay/LetsPlay/Models/GameModel.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 enum Games: String, CaseIterable {
-    case tumok = "튜목"
+    case tumok = "튜 목"
     case matchImage = "짝꿍 찾기"
     case ascendingNum = "순서대로 얍얍"
     case rockSissorPaper = "가위바위보"
