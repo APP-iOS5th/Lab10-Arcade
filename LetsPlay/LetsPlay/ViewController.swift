4fb321311013e4fc5b195c7d13687bfed608a5e2
the-hye <ganzi410@gmail.com>
2024-06-11 13:42:32 +0900

    Initialize Project && Add Assets

---
+++ LetsPlay/LetsPlay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsPlay
+//
+//  Created by JIHYE SEOK on 6/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
