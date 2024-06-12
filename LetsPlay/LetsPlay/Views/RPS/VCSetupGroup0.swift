0c318d307f17a5608661be87e7a496ad9cecfbcd
youngwoo_ahn <codeway119@gmail.com>
2024-06-12 11:36:56 +0900

    modify - navigation

--- LetsPlay/LetsPlay/Views/RPS/VCSetupGroup0.swift
+++ LetsPlay/LetsPlay/Views/RPS/VCSetupGroup0.swift
@@ -3,7 +3,11 @@ import UIKit
 // MARK: - setup - viewGroup0 - GameBackground
 extension RockPaperScissorsViewController {
 	func setupViewGroup0() {
-		setupNavigationBar(title: "가위바위보 대결", leftButtonTitle: "게임목록", leftButtonAction: #selector(leftButtonTappedToList))
+		setupNavigationBar(title: "가위바위보 대결", leftButtonTitle: "게임목록", leftButtonAction: #selector(leftButtonNavigationPop))
 		setupCommonBackgroundImage(view: view, backgroundImageView: backgroundImage)
+	}
+	
+	@objc func leftButtonNavigationPop() {
+		navigationController?.popViewController(animated: true)
 	}
 }
